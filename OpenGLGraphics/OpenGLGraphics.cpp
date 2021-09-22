8a74f3d3046167b8b2b75f99835f20ea24c55806
iSuddenly <issuddenly@gmail.com>
2021-09-22 22:04:39 +0900

    initial commit

---
+++ OpenGLGraphics/OpenGLGraphics.cpp
@@ -0,0 +1,16 @@
+﻿#pragma comment (lib, "lib/freeglut.lib")
+#pragma comment (lib, "lib/glew32.lib")
+
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GL/freeglut_ext.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
